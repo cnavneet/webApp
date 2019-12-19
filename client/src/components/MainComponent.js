5bb881cfeb943a6a84bfad98fb91359c3824b8d8
cnavneet <cnavneet58@gmail.com>
2019-12-19 14:22:15 +0530

    adding the skeleton

---
+++ client/src/components/MainComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className = "App">
+                Hello
+            </div>
+        );
+    }
+}
+
+export default Main;
